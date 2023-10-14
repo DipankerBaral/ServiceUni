285dc1175237fa17baee641faea7e6089c5cb97d
Cipherxiv <tausif-sami@hotmail.com>
2023-10-14 18:47:16 +1100

    adding in endpoints to course.js

--- src/Courses.js
+++ src/Courses.js
@@ -5,7 +5,7 @@ function Courses() {
   const [enrolledCourses, setEnrolledCourses] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3001/getCourses')
+    fetch('https://mongo927-cd142ccce1aa.herokuapp.com/getCourses')
       .then(res => res.json())
       .then(data => {
         console.log("Courses Data:", data);
@@ -20,7 +20,7 @@ function Courses() {
     const loggedInUser = localStorage.getItem("loggedInUser");
     if (loggedInUser) {
       const user = JSON.parse(loggedInUser);
-      fetch(`http://localhost:3001/getEnrolledCourses?username=${user.username}`)
+      fetch(`https://mongo927-cd142ccce1aa.herokuapp.com/getEnrolledCourses?username=${user.username}`)
         .then(res => res.json())
         .then(data => {
           if (data.success) {
@@ -38,7 +38,7 @@ function Courses() {
     if (loggedInUser) {
       const user = JSON.parse(loggedInUser);
       try {
-        const response = await fetch('http://localhost:3001/enroll', {
+        const response = await fetch('https://mongo927-cd142ccce1aa.herokuapp.com/enroll', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ courseId, username: user.username })
@@ -64,7 +64,7 @@ function Courses() {
     if (loggedInUser) {
       const user = JSON.parse(loggedInUser);
       try {
-        const response = await fetch('http://localhost:3001/unenroll', {
+        const response = await fetch('https://mongo927-cd142ccce1aa.herokuapp.com/unenroll', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ courseId, username: user.username })
@@ -90,14 +90,14 @@ function Courses() {
       <h2>Available Courses</h2>
       {courses.length ? (
         <ul>
-          {courses.map(course => (
-            <li key={course.course_id}>
-              {`Course ${course.course_id}: ${course.course_name}`}
-              {enrolledCourses.some(eCourse => eCourse.course_id === course.course_id) 
-                ? <button onClick={() => unenroll(course.course_id)}>Unenroll</button>
-                : <button onClick={() => enroll(course.course_id)}>Enroll</button>}
-            </li>
-          ))}
+            {courses.map(course => (
+              <li key={course.id}>
+                {`Course ${course.id}: ${course.name}`}
+                {enrolledCourses.some(eCourse => eCourse.course_id === course.id) 
+                  ? <button onClick={() => unenroll(course.id)}>Unenroll</button>
+                  : <button onClick={() => enroll(course.id)}>Enroll</button>}
+              </li>
+            ))}
         </ul>
       ) : (
         <p>No courses available.</p>
